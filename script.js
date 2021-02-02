202d09b51ac9d89e4761b1821ac1be6f1124bffa
Dylan Lewis <dlewis62@gatech.edu>
2021-02-01 21:10:42 -0500

    Make mobile usable

--- script.js
+++ script.js
@@ -2,27 +2,34 @@
 function wrapfix(){
     var nn = document.getElementById("nav");
     var cc = document.getElementById("contentblk");
-    if(nn.getBoundingClientRect().top < cc.getBoundingClientRect().top)
-    {
+    if(nn.getBoundingClientRect().top < cc.getBoundingClientRect().top) {
         document.getElementById("navsub").style.float = "left"
         document.getElementById("contentblksub").style.maxWidth = "100%"
     }
-    else
-    {
+    else {
         document.getElementById("navsub").style.float = "right"
-        document.getElementById("contentblksub").style.maxWidth = "66%"
+        document.getElementById("contentblksub").style.maxWidth = "620px"
     }
+
 }
 
 
 window.onload = function() {
-    wrapfix()
+    
     lst = document.getElementById("firstLst").getElementsByTagName("li")
 
     for (var i = 0, len = lst.length; i < len; i++ ) {
         lst[ i ].onmouseover = function() { if(this.id != "projLst") {this.style.color = "rgb(140,140,140)"; this.style.cursor = "pointer"} };
         lst[ i ].onmouseout = function() { if(this.value == 0) {this.style.color="black"} };
     }
+
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+      document.getElementsByTagName( "html" )[0].style[ "font-size" ] = "40px"
+    }else{
+
+    }
+    wrapfix()
 }
 
 
